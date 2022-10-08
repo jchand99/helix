97e8776e219403ddc9c83d773a2501200c91db44
jchand99 <chandlerje@vcu.edu>
2022-10-08 12:36:28 -0400

    Fix svelte tag highlighting

--- runtime/queries/svelte/highlights.scm
+++ runtime/queries/svelte/highlights.scm
@@ -1,68 +1,25 @@
-; Special identifiers
-;--------------------
+; Attributes
 
-; TODO:
-((element (start_tag (tag_name) @_tag) (text) @markup.heading)
- (#match? @_tag "^(h[0-9]|title)$"))
+(attribute (attribute_name) @variable.other.member)
+(attribute (attribute_name ["{" "}"] @punctuation.bracket))
+(attribute "=" @punctuation.delimiter)
+(attribute (expr_attribute_value (expression ["{" "}"] @punctuation.bracket)))
+(attribute (quoted_attribute_value) @string)
 
-((element (start_tag (tag_name) @_tag) (text) @markup.bold)
- (#match? @_tag "^(strong|b)$"))
+; Start tags
 
-((element (start_tag (tag_name) @_tag) (text) @markup.italic)
- (#match? @_tag "^(em|i)$"))
+(start_tag (tag_name) @constructor (#match? @constructor "^[A-Z]"))
+(start_tag (tag_name) @tag)
+(start_tag ["<" ">"] @punctuation.bracket)
 
-; ((element (start_tag (tag_name) @_tag) (text) @markup.strike)
-; (#match? @_tag "^(s|del)$"))
+; Self closing tags
 
-((element (start_tag (tag_name) @_tag) (text) @markup.underline)
- (#eq? @_tag "u"))
+(self_closing_tag (tag_name) @constructor (#match? @constructor "^[A-Z]"))
+(self_closing_tag (tag_name) @tag)
+(self_closing_tag ["<" ">" "/>"] @punctuation.bracket)
 
-((element (start_tag (tag_name) @_tag) (text) @markup.inline)
- (#match? @_tag "^(code|kbd)$"))
+; End tags
 
-((element (start_tag (tag_name) @_tag) (text) @markup.link.url)
- (#eq? @_tag "a"))
-
-((attribute
-   (attribute_name) @_attr
-   (quoted_attribute_value (attribute_value) @markup.link.url))
- (#match? @_attr "^(href|src)$"))
-
-(tag_name) @tag
-(attribute_name) @variable.other.member
-(erroneous_end_tag_name) @error
-(comment) @comment
-
-[
-  (attribute_value)
-  (quoted_attribute_value)
-] @string
-
-[
-  (text)
-  (raw_text_expr)
-] @none
-
-[
-  (special_block_keyword)
-  (then)
-  (as)
-] @keyword
-
-[
-  "{"
-  "}"
-] @punctuation.brackets
-
-"=" @operator
-
-[
-  "<"
-  ">"
-  "</"
-  "/>"
-  "#"
-  ":"
-  "/"
-  "@"
-] @punctuation.definition.tag
+(end_tag (tag_name) @constructor (#match? @constructor "^[A-Z]"))
+(end_tag (tag_name) @tag)
+(end_tag ["</" ">"] @punctuation.bracket)
